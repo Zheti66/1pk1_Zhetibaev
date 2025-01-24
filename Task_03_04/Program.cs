693d050e00fe1ecdb5a4fa259e486a3f1262a204
Еркин Жетибаев <zetibaeverken@gmail.com>
2025-01-25 03:54:19 +0500

    выполнил 4 задание 3 занятия

---
+++ Task_03_04/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Task_03_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int LineCount = 0;
+            do
+            {
+                Console.WriteLine("Ведите текст: ");
+                input = Console.ReadLine();
+                if (!string.IsNullOrEmpty(input) && input.ToLower() != "exit")
+                {
+                    LineCount++;
+                }
+                Console.Clear();
+            }
+            while (!string.IsNullOrEmpty(input) && input.ToLower() != "exit");
+            Console.WriteLine($"Количество введеных строк: {LineCount}");
+
+
+        }
+    }
+}
