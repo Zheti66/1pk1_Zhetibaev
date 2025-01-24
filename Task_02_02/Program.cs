c1959910a13aff2f3c8bb0bb9551809689860452
Еркин Жетибаев <zetibaeverken@gmail.com>
2025-01-25 03:46:59 +0500

    выполнил 2 задание 2 занятия

---
+++ Task_02_02/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task_02_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 8;
+            int b = 14;
+            double c = Math.PI / 4;
+            double one = Math.Pow(a - 1, 1.0 / 3.0);
+            double two = Math.Pow(b + one, 1.0 / 4.0);
+            double three = Math.Abs(a - b);
+            double four = Math.Sin(c);
+            double five = Math.Pow(four, 2);
+            double six = Math.Tan(c);
+
+            double result = two / (three * (five + six));
+            Console.WriteLine(result);
+
+        }
+    }
+}
