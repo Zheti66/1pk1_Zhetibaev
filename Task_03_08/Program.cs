460143c804833699bc84e185a98a3cb0da706d2f
Еркин Жетибаев <zetibaeverken@gmail.com>
2025-02-01 23:56:23 +0500

    решена 8 задача 3 занятия

---
+++ Task_03_08/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task_03_08
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Числа от 20 до 50, делящиеся на 3, но не делящиеся на 5: ");
+            int nums = 20;
+            while (nums <= 50)
+            {
+                if ((nums % 3 == 0) && (nums % 5 != 0))
+                {
+                    Console.Write(nums + " ");
+                }
+                nums++;
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
