c5cfa7a890fd989c4f594a9d70843bdee01349b6
Еркин Жетибаев <zetibaeverken@gmail.com>
2025-03-25 11:34:51 +0500

    задача 1 занятия 20

---
+++ Task_10_05/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task_10_05
+{
+    internal class Program
+    {
+        /*Пользователь вводит целое неотрицательное число n, 
+         * программа с помощью процедурных методов должна 
+         * рассчитать и вывести площадь окружности с радиусом n 
+         * объем и площадь сферы с радиусом n 
+         * 
+         * Осуществите бесконечный цикл для консольного 
+         * ввода числа и вызова метода 
+         * с выводом информации по выходноу параметру 
+         */
+        static void Main(string[] args)
+        {
+           
+        }
+        static void PrintSphereCircleData(int r)
+        {
+            double sphereVolume = 4 / 3.0 * Math.PI * Math.Pow(2, r);
+            double sphereArea = 4 * Math.PI * Math.Pow(r, 2);
+            double circleArea = Math.PI * r * r;
+
+            Console.WriteLine($"Объем сферы: {sphereVolume}");
+            Console.WriteLine($"Площадь сферы: {sphereArea}");
+            Console.WriteLine($"Площадь круга: {circleArea}");
+        }
+    }
+}
---
+++ Task_20_01/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task_20_01
+{
+    internal class Program
+    {
+        /*Создайте enum для дней недели (Monday–Sunday). Напишите программу, которая:
+• Принимает от пользователя число (1–7). 
+• Преобразует его в день недели. 
+• Выводит сообщение:
+• "Будний день" (для Monday–Friday). 
+• "Выходной" (для Saturday, Sunday).
+        */
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Выведите число от 1 до 7 включительно");
+                int UserEnter = Convert.ToInt32(Console.ReadLine());
+                WeekDay day = (WeekDay)UserEnter;
+                Console.WriteLine(day.ToString());
+
+                switch (day)
+                {
+                    case WeekDay.Monday:
+                    case WeekDay.Tuesday:
+                    case WeekDay.Wednesday:
+                    case WeekDay.Thursday:
+                    case WeekDay.Friday:
+                        Console.WriteLine("Будний день");
+                        break;
+                    case WeekDay.Saturday:
+                    case WeekDay.Sunday:
+                        Console.WriteLine("Выходной");
+                        break;
+                    default:
+                        Console.WriteLine("Недопустимое значение");
+                        break;
+                }
+            }
+        }
+    }
+}
